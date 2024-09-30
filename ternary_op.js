6b1dcc929e50cc214c634aae6f1f353a8989665a
Amit Dhyani <amitdhyani0334@gmail.com>
2024-10-01 00:25:03 +0530

    ternary added

---
+++ ternary_op.js
@@ -0,0 +1,18 @@
+// if else and ternary operators using && and ||
+
+let age = 23;
+if(age>=18){
+    console.log('You can watch Berserk');
+}
+else if(age == 0){
+    console.log('Grow up Kidoo');
+}
+else{
+    console.log('You cannot watch Berserk');
+}
+
+// ternary operator
+(condtion) ? true : false;
+
+let name = "Amit";
+name=="Amit  " ? console.log('Name is correct') : console.log('Name is wrong');
