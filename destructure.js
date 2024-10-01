c844d5e5f61e1d39258c6b853cc173036fc99bad
Amit Dhyani <amitdhyani0334@gmail.com>
2024-10-02 00:06:54 +0530

    array destructure

---
+++ destructure.js
@@ -0,0 +1,11 @@
+// array destructuring 
+
+let arr = [1,2,3,4,5];
+let [a,b,c] = arr;
+console.log(a,b,c);
+
+let user = ["Amit", 23, "WebDev"];
+let [name, age, role] = user;
+console.log(name, age, role);
+
+
