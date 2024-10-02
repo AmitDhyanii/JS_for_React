25b20af8947ec09e360993e4703a8680eb41221c
Amit Dhyani <amitdhyani0334@gmail.com>
2024-10-02 23:59:27 +0530

    spread op

---
+++ spread_rest.js
@@ -0,0 +1,7 @@
+// function sumOne(a, b){
+//     return a + b;
+// }
+// let myArr = [2, 3];
+// console.log(sumOne(...myArr));   // it spread the arr values like 2 3
+// console.log(sumOne(2,3));
+
