a4cc20bfec4583a391216cbd0dff85745868387e
Amit Dhyani <amitdhyani0334@gmail.com>
2024-10-04 00:01:16 +0530

    op chain again

---
+++ optional_chaining.js
@@ -0,0 +1,14 @@
+// optional chaining => ?
+// we use it in deeply nested object or array if we dont know value exist which we want to access
+
+// const user = {
+//     name: "Amit",
+//     age: 23,
+//     address: {
+//         road: "Bass Road",
+//         city: "Dharuhera",
+//         state: "Haryana",
+//         pincode: 123106
+//     }
+// }
+// console.log(user.address.district?.place);
