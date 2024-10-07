03d7cbf79f0279287fffa963be5b3852743966f7
Amit Dhyani <amitdhyani0334@gmail.com>
2024-10-08 00:15:38 +0530

    promise code

--- promise.js
+++ promise.js
@@ -2,3 +2,16 @@
 // 3 states = pending || fullfilled || rejected
 
 
+// 1st promise
+const promiseOne = new Promise(function(resolve, reject){
+    // Do an async task
+    // DB calls, Cryptography, Networking
+    setTimeout(function(){
+        console.log('Async task 1 is completed');
+        resolve();
+    }, 1000);
+});
+
+promiseOne.then(function(){
+    console.log('Promise 1 resolved');
+});
