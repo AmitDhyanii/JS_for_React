2b859067053c2df282147863e961ea2eb83da60f
Amit Dhyani <amitdhyani0334@gmail.com>
2024-10-09 00:02:59 +0530

    promise 2

--- promise.js
+++ promise.js
@@ -15,3 +15,14 @@ const promiseOne = new Promise(function(resolve, reject){
 promiseOne.then(function(){
     console.log('Promise 1 resolved');
 });
+
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log('Async task 2 is completed');
+        resolve();
+    }, 1000);
+}).then(function(){
+    console.log('Promise 2 resolved');
+});
+
